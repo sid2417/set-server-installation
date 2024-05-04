83271477581018263bcb41596ade92c8cc179cfa
sid2417 <siddhartha.java9398@gmail.com>
2024-05-04 15:58:17 +0530

    Set mysql installation

--- current.sh
+++ current.sh
@@ -4,6 +4,8 @@ echo "This is current script"
 VARIABLE="current--script"
 echo $VARIABLE
 echo "now current script is running..."
+echo "this is current script: $$"
+echo "this is current script: $0"
 
 ./other.sh
 
