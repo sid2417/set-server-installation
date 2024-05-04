43cd8cdf00269744048587a763d574f290a326f7
sid2417 <siddhartha.java9398@gmail.com>
2024-05-04 15:45:32 +0530

    Set mysql installation

--- other.sh
+++ other.sh
@@ -7,3 +7,12 @@ echo $VARIABLE
 
 echo "this is other script: $$"
 echo "this is other script: $0"
+
+echo "Printing Everything : $@"
+echo "Number of arguments you are passing : $#"
+echo "This is current working directory : $PWD"
+echo "Which user is using this : $USER"
+echo "Current server name : $HOSTNAME"
+echo "This is home directory : $HOME"
+echo "This is current file name : $0"
+echo "This is showing process id of current shell script program (PID): $$"
